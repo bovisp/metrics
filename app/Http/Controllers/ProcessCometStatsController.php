7b8294af0ee2fe4480325ac267e3e649b6cb49b4
Paul Bovis <paul.bovis@canada.ca>
2019-11-05 11:24:00 -0700

    Create COMET stat mailable

--- app/Http/Controllers/ProcessCometStatsController.php
+++ app/Http/Controllers/ProcessCometStatsController.php
@@ -4,18 +4,30 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use App\CometCourse;
+use App\Mail\CometStats;
 use App\Classes\CometReport;
 use App\Exports\CometExport;
+use App\Jobs\ProcessCometStats;
+use Illuminate\Support\Facades\Mail;
 use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Support\Facades\Storage;
 
 class ProcessCometStatsController extends Controller
 {
     public function index()
     {
-      $stats = (new CometReport('january 1 2015', 'september 1 2019'))->process();
+      $start = 'january 1 2015';
 
-      $filename = 'comet_stats_' . (new Carbon('january 1 2015'))->format('Ymd') . '_' . (new Carbon('september 1 2019'))->format('Ymd') . '.xlsx';
+      $end = 'september 1 2019';
+
+      $stats = (new CometReport($start, $end))->process();
+
+      $filename = 'comet_stats_' . (new Carbon($start))->format('Ymd') . '_' . (new Carbon($end))->format('Ymd') . '.xlsx';
 
       Excel::store(new CometExport($stats), $filename);
+
+      Mail::to(request()->user())->send(new CometStats($start, $end, $filename));
+
+      Storage::delete($filename);
     }
 }
